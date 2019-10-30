cb057aa5d4580ab58e648af60fa67beb15719ed2
Jaydeep Singh <43669157+Jdsingh17@users.noreply.github.com>
2019-10-30 05:41:25 -0700

    Add files via upload

---
+++ s2t.py
@@ -0,0 +1,18 @@
+#aurthor: Rishi Prajapati (leftyyyy)
+#make sure to have following packages:
+#1. PyAudio (conda install PyAudio)
+#2. googleAPIpython (conda install -c conda-forge google-api-python-client)
+#3. speech_recognition (conda install SpeechRecognition)
+def abk():
+    import speech_recognition as sr
+    rec=sr.Recognizer()
+    with sr.Microphone as source:
+        print("bol bosdk")
+        audio = r.listen(source)
+        try:
+            #passing it to googleAPI
+            txt = r.recognize_google(audio)
+            arr=[str(_) for _ in txt.split()]
+            return arr
+        except:
+            print("firse bol bosdk")
