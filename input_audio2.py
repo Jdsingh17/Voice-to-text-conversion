3ea82419e07864573413f4100c5c85378daedea5
Jaydeep Singh <43669157+Jdsingh17@users.noreply.github.com>
2019-10-31 01:04:51 -0700

    Update input_audio2.py

--- input_audio2.py
+++ input_audio2.py
@@ -1,22 +1,36 @@
 import pyaudio
-import numpy as np
-from matplotlib import pyplot as plt
-
-CHUNKSIZE = 1024 # fixed chunk size
-
-# initialize portaudio
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=CHUNKSIZE)
-
-# do this as long as you want fresh samples
-data = stream.read(CHUNKSIZE)
-numpydata = np.frombuffer(data, dtype=np.int16)
-
-# plot data
-plt.plot(numpydata)
-plt.show()
-
-# close stream
+import wave
+ 
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "file.wav"
+ 
+audio = pyaudio.PyAudio()
+ 
+# start Recording
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                rate=RATE, input=True,
+                frames_per_buffer=CHUNK)
+print "recording..."
+frames = []
+ 
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+print "finished recording"
+ 
+ 
+# stop Recording
 stream.stop_stream()
 stream.close()
-p.terminate()
+audio.terminate()
+ 
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
