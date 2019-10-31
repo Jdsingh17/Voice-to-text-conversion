d3c3a95fd7fe90df9ff8348d1570fe5422984e73
Jaydeep Singh <43669157+Jdsingh17@users.noreply.github.com>
2019-10-30 20:31:11 -0700

    Create s2t2.py

---
+++ s2t2.py
@@ -0,0 +1,34 @@
+'''
+Created on 30-10-2019
+@author : lucifer
+Instructions
+-----------------
+1. pip install SpeechRecognition
+2. sudo apt-get install pyaudio
+'''
+
+import speech_recognition as sr
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8,GPIO.OUT)
+def LedBlinkABK():
+	while True:
+		GPIO.output(8,GPIO.HIGH)
+		sleep(1)
+		GPIO.output(8,GPIO.LOW)
+		sleep(1)
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+	print("Ayy Bolo Naa : ")
+	audio = r.listen(source)
+
+	try:
+		text = r.recognize_google(audio)
+		print("You said : {}".format(text))
+		LedBlinkABK()
+	except:
+		print("Ayy Phir se Bolo naa")
